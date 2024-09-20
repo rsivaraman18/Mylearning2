e8720ac4065fb518c705e1e164297e9362bac9df
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-20 13:39:31 +0530

    Auto commit for 2_Polymorphism/4_Method_Overriding.py

---
+++ 2_Polymorphism/4_Method_Overriding.py
@@ -0,0 +1,19 @@
+"""
+Method Overridding: It is possible only with Inheritance.
+                    Here the method with same params have to present in both the class
+
+"""
+class Father:
+    def sleep(self):
+        print("Sleeps at 8 pm")
+
+    def eats(self):
+        print("He cooks and eat")
+
+class Son(Father):
+    def sleep(self):
+        print("Sleeps at 9 pm")
+
+person = Son()
+person.eats()
+person.sleep()
