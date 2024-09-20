add185361cf73a540672ce2d1a46ecd8b50a343b
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-20 10:30:45 +0530

    Auto commit for 2_Polymorphism/1_Duck_Eg1.py

--- 2_Polymorphism/1_Duck_Eg1.py
+++ 2_Polymorphism/1_Duck_Eg1.py
@@ -1,5 +1,49 @@
 # Polymorphism Concept Ducktyping will be learned here.
 
-print(1+2)
-print("1")
+### Example -1 --> We dont need to declare the x type as int or float
+def square(x):
+    return x*x
+
+print(square(5) )   
+print(square(5.5))
+
+
+
+
+### Example 2 -->
+
+class Duck:
+    def swim(self):
+        print("I am a duck and I can swim")
+    
+    def speaks(self):
+        print("Quack Qauck")
+    
+
+class Dog:
+    def swim(self):
+        print("I am a dog and I can swim")
+
+    def speaks(self):
+        print("woo woo")
+    
+
+class Person:
+    def speaks(self):
+        print("blah blah blah")
+
+
+def display(obj):
+    obj.swim()
+    obj.speaks()
+    print("Thank you...")
+
+### Object Creation for Classes
+obj1 = Duck()
+obj2 = Dog()
+obj3 = Person()
+
+
+
+
 
