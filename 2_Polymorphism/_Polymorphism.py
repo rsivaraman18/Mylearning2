c68226ebf54159b2a2e3211e1b7efb8ca294774b
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-20 12:09:21 +0530

    Auto commit for 2_Polymorphism/_Polymorphism.py

---
+++ 2_Polymorphism/_Polymorphism.py
@@ -0,0 +1,8 @@
+## Example 1: Same '+' Operator Works Different
+print(1+2)
+print("1"+"2")
+
+## Example 2: 'len' works different
+print(len('siva'))
+print( len({'name':'siva','age':23,}))
+print(len([1,23,3,5]))
