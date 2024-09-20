68fdc9ea3c97cd460e091110546058fe9cdaabad
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-20 16:09:41 +0530

    Auto commit for 5_MagicMethods/1_magic.py

--- 5_MagicMethods/1_magic.py
+++ 5_MagicMethods/1_magic.py
@@ -1,15 +1,29 @@
 # Magic methods also known as Dunder Method.
 
+from typing import Any
+
+
 print("Hiiii")
 class Author:
 
-    def __init__(self,book,author) :
+    def __init__(self,book,author,page) :
         self.book = book
         self.author = author
+        self.page = page
     
-    # def __str__(self):
-    #     return f"{self.book} written by {self.author}"
+    def __str__(self):
+        return f"{self.book} written by {self.author}"
+    
+    def __len__(self):
+        return self.page
+    
+    def __call__(self) :
+        return "Now I am Callable"
+    
+    
 
-p = Author("Agni Siragu","APJ")
+p = Author("Agni Siragu","APJ",120)
 print(p)
 print(str(p))
+print(len(p))
+print(p())
