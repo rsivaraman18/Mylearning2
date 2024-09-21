0e20028cda0c6ff8df9af876986df6a31d71aa26
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-21 15:31:01 +0530

    Auto commit for 4_Encapsulation/3_getter.py

--- 4_Encapsulation/3_getter.py
+++ 4_Encapsulation/3_getter.py
@@ -7,17 +7,18 @@ class Student:
     def get_age(self):
         return f"{self.name} age is {self.__age}"
     
-    def set_age(self):
-        if self.__age >20:
-            return self.__age
+    def set_age(self,newage):
+        if newage > 35:
+            print(f"{newage} is Invalid ")
         else:
-            return "You are too young"
+            self.__age = newage
+            
 
 obj = Student("Python",1001,20)
 print(obj.name) 
 print(obj._roll)
 print(obj.get_age())
 ## Set age
-
-print(obj.set_age())
+obj.set_age(27)
+print(obj.get_age())
     
