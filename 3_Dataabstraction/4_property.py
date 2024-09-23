02aec5e33c4a5e1b114cf965a3869c84d9e063b5
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-23 11:37:21 +0530

    Auto commit for 3_Dataabstraction/4_property.py

---
+++ 3_Dataabstraction/4_property.py
@@ -0,0 +1,42 @@
+class BankAccount:
+    def __init__(self, balance):
+        self._balance = balance  # Protected attribute
+
+    @property
+    def balance(self):
+        # Getter for balance
+        print('Gets the Balance')
+        return self._balance
+    
+    @balance.setter
+    def balance(self, amount):
+        # Setter for balance with validation
+        print('Setting Balance')
+        if amount >= 0:
+            self._balance = amount
+        else:
+            raise ValueError("Balance cannot be negative")
+    
+    def deposit(self, amount):
+        self.balance += amount  # This uses the setter to update balance
+
+    def withdraw(self, amount):
+        if self._balance >= amount:
+            self.balance -= amount  # This uses the setter to update balance
+        else:
+            raise ValueError("Insufficient funds")
+
+# Creating an instance
+account = BankAccount(1000)
+
+# Accessing the balance using the getter
+print("Initial Balance:", account.balance)
+
+# Depositing and withdrawing
+account.deposit(500)
+print("Balance after deposit:", account.balance)
+
+account.withdraw(300)
+print("Balance after withdrawal:", account.balance)
+
+# account.balance = -500  # Raises ValueError: Balance cannot be negative
