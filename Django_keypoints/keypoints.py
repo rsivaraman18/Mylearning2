1a4886fa9ca1336181dbd8ad2a4860b1ecf73142
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-24 11:02:52 +0530

    Auto commit for Django_keypoints/keypoints.py

--- Django_keypoints/keypoints.py
+++ Django_keypoints/keypoints.py
@@ -8,3 +8,18 @@ data.save()
 obj = Member(name='java')
 obj.update()
 
+# To Update 4th record in a queryset
+# from members.models import Member
+# x = Member.objects.all()[4]
+# x.name = 'java'
+# x.save()
+
+obj = Member.objects.filter(status="inactive")
+obj.update(status="active")
+
+data.delete()
+
+
+from members.models import Member
+x = Member.objects.all()[2]
+x.delete()
