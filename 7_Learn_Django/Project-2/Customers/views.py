d73825b7017563143e3ebd5259a88f62b66cc92a
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-27 16:27:05 +0530

    Auto commit for 7_Learn_Django/Project-2/Customers/views.py

--- 7_Learn_Django/Project-2/Customers/views.py
+++ 7_Learn_Django/Project-2/Customers/views.py
@@ -3,18 +3,28 @@ from Customers. models import *
 from django.contrib import messages
 from . forms import *
 
+"""Here Everything Done with Dual Method"""
+
 def CustomerAdd(request):
     cust_form = MyCustomerForm()
     if request.method == 'POST':
-        print(request.POST)
+        """Method 1"""
         cname     = request.POST['Customer_name']
         csince    = request.POST['Customer_since']
         clocation = request.POST['Customer_location']
-
         newcustomer = MyCustomer(Customer_name=cname,Customer_since=csince,Customer_location=clocation,)
         newcustomer.save()
         return redirect('/Customers/customer_viewall/')
+    return render(request,'Customer/1_add.html',{'cust_form':cust_form})
 
+
+def CustomerAdd(request):
+    cust_form = MyCustomerForm()
+    if request.method == 'POST':
+        """Method 2- Using Form """
+        newcustomer = MyCustomerForm(request.POST)
+        newcustomer.save()
+        return redirect('/Customers/customer_viewall/')
     return render(request,'Customer/1_add.html',{'cust_form':cust_form})
 
 
@@ -32,6 +42,12 @@ def CustomerDelete(request,id):
     messages.success(request,f'Customer {customerdetail.Customer_name} ID{id} Deleted')
     return redirect('/Customers/customer_viewall/')
 
-def CustomerUpdate(request):
-    return render(request,'Customer/1_add.html')
+def CustomerUpdate(request,id):
+    cust_oldinfo = MyCustomer.objects.get(id=id)
+    cust_form    = MyCustomerForm(instance=cust_oldinfo)
+    if request.method=='POST':
+        update_cust = MyCustomerForm(request.POST,isinstance=cust_oldinfo)
+        update_cust.save()
+
+    return render(request,'Customer/3_update.html',{'cust_form':cust_form})
     
