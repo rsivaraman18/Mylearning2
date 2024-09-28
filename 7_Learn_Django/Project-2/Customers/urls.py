63f130d8458d98ae327c35b7a95c6ab007ba13e4
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-28 13:29:45 +0530

    Auto commit for 7_Learn_Django/Project-2/Customers/urls.py

--- 7_Learn_Django/Project-2/Customers/urls.py
+++ 7_Learn_Django/Project-2/Customers/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('customer_viewall/', views.CustomerViewall , name='customerlist'),
     path('customer_delete/<int:id>/',  views.CustomerDelete),
     path('customer_update/<int:id>/',  views.CustomerUpdate),
+
+    
+    path('customer_vieworders/',  views.CustomerViewOrder , name='orderslist'),
+    path('customer_neworder/',   views.CustomerNewOrder),
+    path('customer_orderdelete/<int:id>/',   views.CustomerOrderDelete),
+
 ]
