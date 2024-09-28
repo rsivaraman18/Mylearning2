ec90d0d3cff81fefad94bfc7de034dd09fe078d5
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-28 13:19:41 +0530

    Auto commit for 7_Learn_Django/Project-2/Customers/forms.py

--- 7_Learn_Django/Project-2/Customers/forms.py
+++ 7_Learn_Django/Project-2/Customers/forms.py
@@ -8,4 +8,10 @@ class MyCustomerForm(ModelForm):
         fields = '__all__'
 
 
+class MyOrderForm(ModelForm):
+    class Meta:
+        model = Myorders
+        # fields = '__all__'
+        fields = ['customer_reference','product_reference','order_number','order_date','quantity']
+
 
