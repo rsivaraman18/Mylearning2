ed58facf032393675c5ea11a1ad6d9694901dd99
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-28 10:58:10 +0530

    Auto commit for 7_Learn_Django/Project-2/Products/models.py

--- 7_Learn_Django/Project-2/Products/models.py
+++ 7_Learn_Django/Project-2/Products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class MyProducts(models.Model):
+    product_name = models.CharField(max_length=35,null=True)
+    product_code = models.CharField(max_length=35,null=True)
+    product_price = models.FloatField(default=0)
+    product_gst  = models.IntegerField(default=0)
+    food_product = models.BooleanField(default=0)
+
+
+    def __str__(self):
+        return f"{self.product_name}  {self.product_price}"
