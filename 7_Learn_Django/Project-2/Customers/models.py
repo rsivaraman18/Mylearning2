383be53c3bfd87291e4abb6f861a1a6e5716d555
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-28 10:38:03 +0530

    Auto commit for 7_Learn_Django/Project-2/Customers/models.py

--- 7_Learn_Django/Project-2/Customers/models.py
+++ 7_Learn_Django/Project-2/Customers/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from Products.models import *
 
 class MyCustomer(models.Model):
     Customer_name = models.CharField(max_length=35,null=True)
@@ -10,3 +10,15 @@ class MyCustomer(models.Model):
         return self.Customer_name
 
 
+class Myorders(models.Model):
+    customer_reference = models.ForeignKey(MyCustomer, on_delete=models.CASCADE , null=True)
+    product_reference  = models.ForeignKey(MyProducts, on_delete=models.SET_NULL, null=True)
+    order_number       = models.CharField(max_length=20 , null= True)
+    order_date         = models.DateField(null=True)
+    quantity           = models.FloatField(default=0)
+    amount             = models.FloatField(default=0)
+    gst_number         = models.FloatField(default=0)
+    bill_amount        = models.FloatField(default=0)
+
+    def __str__(self):
+        return f'{self.order_number} - Amount Rs {self.amount}'
